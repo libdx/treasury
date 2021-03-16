1627244ae72b9eaed2f3e4c1ff3f9c28c6887da8
Oleksandr Ignatenko <avi.cpp@gmail.com>
2021-03-16 15:02:02 +0100

    Remove dummy `TreasureHuntQueryParamsView` class

--- project/apps/api/views.py
+++ project/apps/api/views.py
@@ -54,30 +54,3 @@ class AttemptViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Attempt.objects.all()
     serializer_class = AttemptSerializer
-
-
-class TreasureHuntQueryParamsView(APIView):
-    """Represents 'treasure_hunt.json' endpoint with handling query params."""
-
-    permission_classes = [AllowAny]
-
-    def post(self, request, format=None):
-        loc = request.query_params.getlist("loc[]")
-        data = {
-            "user": str(request.user),
-            "auth": str(request.auth),
-            "loc": loc,
-            "request.data": request.data,
-        }
-
-        if len(loc) < 2:
-            data = {
-                "status": "error",
-                "message": "loc[] query params requires 2 elements",
-            }
-            return Response(
-                status=400,
-                data=data,
-            )
-
-        return Response(data)
