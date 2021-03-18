e813658bff49572ca2372021a8658f473c597cef
Oleksandr Ignatenko <avi.cpp@gmail.com>
2021-03-18 10:31:59 +0100

    Now CELERY_BROKER_URL is taked from BROKER_URL env variable

--- project/settings.py
+++ project/settings.py
@@ -161,6 +161,6 @@ AWS_SES_EMAIL_SOURCE = os.getenv("AWS_SES_EMAIL_SOURCE")
 
 # Celery
 CELERY_RESULT_BACKEND = "rpc://"
-CELERY_BROKER_URL = "pyamqp://rabbitmq:rabbitmq@broker//"
+CELERY_BROKER_URL = os.getenv("BROKER_URL")
 # To compline with free plan on Heroku:
 CELERY_BROKER_POOL_LIMIT = 1
